da3a7cba098719659df2913ffcf27a76372a803d
Sherreka <rekadarnb@gmail.com>
2022-07-15 17:21:08 -0400

    adding more functionality

--- server.js
+++ server.js
@@ -14,12 +14,21 @@ app.use(express.urlencoded({extended: false}));
 
 app.get('/', async (req, res) => {
     const shortUrls = await ShortUrl.find()
-    res.render('index', { shortUrls: short });
+    res.render('index', { shortUrls: shortUrls});
 });
 
 app.post('/shortUrls', async (req, res) => {
    await ShortUrl.create({ full: req.body.fullUrl})
    res.redirect('/')
 });
+
+app.get('/:shortUrl', async (req, res) => {
+   const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+   if (shortUrl == null) res.sendStatus(404)
+
+   shortUrl.clicks++
+   shortUrl.save()
+   res.redirect(shortUrl.full)
+})
 
 app.listen(process.env.PORT || 3000);
