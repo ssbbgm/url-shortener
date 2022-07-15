73c502dd4e0a09bf5a8161bda9370f7a68228b76
Sherreka <rekadarnb@gmail.com>
2022-07-15 16:38:49 -0400

    added model info

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb://localhost/urlShortner', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
