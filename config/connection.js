4cb6fd41a8a73def262afa093c8b88da3eb752f8
Sherreka <rekadarnb@gmail.com>
2022-07-18 17:58:08 -0400

    adding const value in connection file

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 const { connect, connection } = require('mongoose');
 // const mongoose = require('mongoose');
 
-let MONGODB_URI = 'mongodb://localhost/urlShortner' || process.env.MONGODB_URI;
+const MONGODB_URI = 'mongodb://localhost/urlShortner' || process.env.MONGODB_URI;
 
 connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-module.exports = connect; 
+module.exports = connection; 
