111426f31009ef66e2c77758c46ee5fd61a25c6c
Sherreka <rekadarnb@gmail.com>
2022-07-19 18:17:17 -0400

    changed port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.set('view engine', 'ejs');
